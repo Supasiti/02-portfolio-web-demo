5b62ed58983041011b3402f19481e6a6fa60e389
Thara Supasiti <sapiensis@gmail.com>
2021-07-24 13:03:17 +1000

    refactor to display all projects

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,60 +1,3 @@
-const portfolioData = [
-  {
-    title : "Cryptfolio",
-    imgData : {
-      source : [
-        './assets/images/cryptfolio-400w.jpeg',
-        './assets/images/cryptfolio-800w.jpeg'
-      ],
-      alt : "screenshot of Cryptfolio"
-    },
-    tags : [
-      'HTML5',
-      'CSS',
-      'JavaScript',
-      'Bulma'
-    ],
-    link : "https://lallender.github.io/Cryptfolio/",
-    codeLink : "https://github.com/LAllender/Cryptfolio",
-    description : 'An app that allows the user to see visually the value of their cryptocurrency portfolio.'
-  },
-  {
-    title : "Weather Dashboard",
-    imgData : {
-      source : [
-        './assets/images/weather-dashboard-400w.png',
-        './assets/images/weather-dashboard-800w.png',
-      ],
-      alt : "screenshot of Weather Dashboard"
-    },
-    tags : [
-      'HTML5',
-      'CSS',
-      'JavaScript',
-    ],
-    link : "https://supasiti.github.io/06-weather-dashboard/",
-    codeLink : "https://github.com/Supasiti/06-weather-dashboard",
-    description : 'A weather dashboard that allows users to get current and the next 5-day weather data for a city they are interested in.'
-  },
-  {
-    title : "Code Quiz",
-    imgData : {
-      source : [
-        './assets/images/code-quiz-400w.gif',
-        './assets/images/code-quiz-800w.gif',
-      ],
-      alt : "screenshot of Code Quiz"
-    },
-    tags : [
-      'HTML5',
-      'CSS',
-      'JavaScript',
-    ],
-    link : "https://supasiti.github.io/04-code-quiz/",
-    codeLink : "https://github.com/Supasiti/04-code-quiz",
-    description : 'A lightweight quiz app that tests your knowledge on JavaScript.'
-  }
-];
 
 // open menu on mobile view port
 const connectOpenMenuOnMobile = () => {
@@ -133,10 +76,7 @@ const connectOpenMenuOnTablet = () => {
 };
 
 
-// displayPortfolio(portfolioData);
+displayAllProjects();
 connectOpenMenuOnMobile();
 connectOpenMenuOnTablet();
 
-
-const containerEl = document.querySelector('#project-container');
-containerEl.appendChild(createProjectCard(portfolioData[0])); 
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,75 +1,93 @@
-
-//  remove all elements in the list
-const removeAllElements = (els) => {
-  if (els.length > 0){
-    els.forEach(el => el.remove());
-  };
-}
-
-// remove all children of an element
-const removeAllChildren = (containerEl) => {
-  const childEls = [...containerEl.children];
-  removeAllElements(childEls);
-}
-
-// create container for the first project
-const createFirstProjectContentDiv = () => {
-  const result = document.createElement('div');
-  result.classList = 'px-2';
-  return result;
-};
-
-// display first project on screen
-const displayFirstProject = (projectData) => {
-  const containerEl = document.querySelector('#first-project-container');
-  const contentEl = createFirstProjectContentDiv();
-  const cardEl = createCard(projectData);
-
-  removeAllChildren(containerEl);
-
-  contentEl.appendChild(cardEl);
-  containerEl.appendChild(contentEl);
-}
+const portfolioData = [
+  {
+    title : "Cryptfolio",
+    imgData : {
+      source : [
+        './assets/images/cryptfolio-400w.jpeg',
+        './assets/images/cryptfolio-800w.jpeg'
+      ],
+      alt : "screenshot of Cryptfolio"
+    },
+    tags : [
+      'HTML5',
+      'CSS',
+      'JavaScript',
+      'Bulma'
+    ],
+    link : "https://lallender.github.io/Cryptfolio/",
+    codeLink : "https://github.com/LAllender/Cryptfolio",
+    description : 'An app that allows the user to see visually the value of their cryptocurrency portfolio.'
+  },
+  {
+    title : "Weather Dashboard",
+    imgData : {
+      source : [
+        './assets/images/weather-dashboard-400w.png',
+        './assets/images/weather-dashboard-800w.png',
+      ],
+      alt : "screenshot of Weather Dashboard"
+    },
+    tags : [
+      'HTML5',
+      'CSS',
+      'JavaScript',
+    ],
+    link : "https://supasiti.github.io/06-weather-dashboard/",
+    codeLink : "https://github.com/Supasiti/06-weather-dashboard",
+    description : 'A weather dashboard that allows users to get current and the next 5-day weather data for a city they are interested in.'
+  },
+  {
+    title : "Code Quiz",
+    imgData : {
+      source : [
+        './assets/images/code-quiz-400w.gif',
+        './assets/images/code-quiz-800w.gif',
+      ],
+      alt : "screenshot of Code Quiz"
+    },
+    tags : [
+      'HTML5',
+      'CSS',
+      'JavaScript',
+    ],
+    link : "https://supasiti.github.io/04-code-quiz/",
+    codeLink : "https://github.com/Supasiti/04-code-quiz",
+    description : 'A lightweight quiz app that tests your knowledge on JavaScript.'
+  }
+];
 
 // --------------------
 //  for other projects
 
 // ul to hold all the projects
-const createOtherProjectContentUL = () => {
+const createProjectContent = () => {
   const result = document.createElement('ul');
   result.classList = 'flex-row align-stretch';
   return result;
 };
 
 // <li> element to hold each project 
-const createListEl = () => {
+const createProjectListItem = () => {
   const result = document.createElement('li');
-  result.classList = 'px-2 col-12 col-lg-6 flex-row';
+  result.classList = 'px-2 col-12 flex-row';
   return result;
 };
 
 
 // display other project in a grid
-const displayOtherProject = (projects) => {
-  const containerEl = document.querySelector('#other-project-container');
-  const contentEl = createOtherProjectContentUL();
+const displayAllProjects = () => {
+  const containerEl = document.querySelector('#project-container');
+  const contentEl = createProjectContent();
 
   removeAllChildren(containerEl);
 
-  projects.map(project => {
-    const liEl = createListEl();
-    const cardEl = createCard(project);
-    liEl.appendChild(cardEl);
+  portfolioData.map(project => {
+    const liEl = createProjectListItem();
+    liEl.appendChild(createProjectCard(project));
     contentEl.appendChild(liEl);
   });
   containerEl.appendChild(contentEl);
 }
 
 
-//---------------------
-// Display portfolio
 
-const displayPortfolio = (portfolioData) => {
-  displayFirstProject(portfolioData[0]);
-  displayOtherProject(portfolioData.slice(1));
-};
